246d4e52985e984e1078bbea80f30609cf332fcf
Sirvisetti Bala Pavan <sirvisettibalapavan@Sirvisettis-MacBook-Air.local>
2026-07-26 23:05:25 +0530

    Modified function from bala branch

--- tests/newFeature.spec.ts
+++ tests/newFeature.spec.ts
@@ -2,4 +2,6 @@ import {test, Page} from '@playwright/test'
 
 test('new feature on boarded ', async({page})=>{
     console.log('new file is created  for new feature');
+
+    console.log('updated from bala branch');
 } );
